a973b0de5d898f09474fc98d7d3ab7feacb41322
Xavier Perseguers <xavier@causal.ch>
2020-10-30 15:09:53 +0100

    [BUGFIX] Fix dependency cycle in TYPO3 v10

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -6,9 +6,14 @@ return [
             'after' => [
                 'typo3/cms-frontend/authentication',
             ],
-            'before' => [
-                'typo3/cms-frontend/backend-user-authentication',
-            ],
+            'before' =>
+                version_compare((new \TYPO3\CMS\Core\Information\Typo3Version())->getBranch(), '10.4', '<')
+                    ? [
+                    'typo3/cms-frontend/backend-user-authentication',
+                ]
+                    : [
+                    'typo3/cms-frontend/base-redirect-resolver',
+                ],
         ],
     ],
 ];
