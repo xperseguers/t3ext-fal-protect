549111fc60bf60d4b66784929d16417290da961b
Xavier Perseguers <xavier@causal.ch>
2020-11-18 17:09:48 +0100

    [BUGFIX] Reorder Middleware in TYPO3 v9 for proper error handling
    Resolves: #21

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -5,11 +5,12 @@ return [
             'target' => \Causal\FalProtect\Middleware\FileMiddleware::class,
             'after' => [
                 'typo3/cms-frontend/authentication',
+                'typo3/cms-frontend/site',
             ],
             'before' =>
                 version_compare((new \TYPO3\CMS\Core\Information\Typo3Version())->getBranch(), '10.4', '<')
                     ? [
-                    'typo3/cms-frontend/backend-user-authentication',
+                    'typo3/cms-frontend/preview-simulator',
                 ]
                     : [
                     'typo3/cms-frontend/base-redirect-resolver',
