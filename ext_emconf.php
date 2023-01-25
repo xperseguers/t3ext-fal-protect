af2ea85656669cf36ac91a572a9a2e9f7556dec9
Xavier Perseguers <xavier@causal.ch>
2023-01-25 14:31:44 +0100

    [TASK] Mark extension as compatible with PHP 8.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Causal Sàrl',
     'constraints' => [
         'depends' => [
-            'php' => '7.2.0-8.1.99',
+            'php' => '7.2.0-8.2.99',
             'typo3' => '9.5.0-11.5.99'
         ],
         'conflicts' => [],
