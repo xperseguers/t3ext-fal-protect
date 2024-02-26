141b5391837ec936c853b25045146161fa138b49
Xavier Perseguers <xavier@causal.ch>
2024-02-26 22:31:10 +0100

    [TASK] Use a white icon in File > Filelist since TYPO3 v12

--- ext_tables.php
+++ ext_tables.php
@@ -3,11 +3,14 @@ defined('TYPO3_MODE') || defined('TYPO3') || die();
 
 (static function (string $_EXTKEY) {
     $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    $typo3Version = (new \TYPO3\CMS\Core\Information\Typo3Version())->getBranch();
     $iconRegistry->registerIcon(
         'actions-protect-folder',
         \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
         [
-            'source' => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/protect-folder.svg',
+            'source' => version_compare($typo3Version, '12.4', '>=')
+                ? 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/protect-folder-v12.svg'
+                : 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/protect-folder.svg',
         ]
     );
 })('fal_protect');
