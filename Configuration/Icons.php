4bd8cd32bdf0fe7595f9fbdf6075a780816a7764
Xavier Perseguers <xavier@causal.ch>
2024-07-12 10:06:20 +0200

    [TASK] Migrate registration with IconRegistry to Configuration/Icons.php

---
+++ Configuration/Icons.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types = 1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+use TYPO3\CMS\Core\Information\Typo3Version;
+
+return [
+    'actions-protect-folder' => [
+        'provider' => SvgIconProvider::class,
+        'source' => (new Typo3Version())->getMajorVersion() >= 12
+            ? 'EXT:fal_protect/Resources/Public/Icons/protect-folder-v12.svg'
+            : 'EXT:fal_protect/Resources/Public/Icons/protect-folder.svg',
+    ],
+];
--- ext_tables.php
+++ ext_tables.php
@@ -2,15 +2,14 @@
 defined('TYPO3_MODE') || defined('TYPO3') || die();
 
 (static function (string $_EXTKEY) {
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-    $typo3Version = (new \TYPO3\CMS\Core\Information\Typo3Version())->getMajorVersion();
-    $iconRegistry->registerIcon(
-        'actions-protect-folder',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        [
-            'source' => $typo3Version >= 12
-                ? 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/protect-folder-v12.svg'
-                : 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/protect-folder.svg',
-        ]
-    );
+    if ((new \TYPO3\CMS\Core\Information\Typo3Version())->getMajorVersion() < 11) {
+        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+        $iconRegistry->registerIcon(
+            'actions-protect-folder',
+            \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+            [
+                'source' => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/protect-folder.svg',
+            ]
+        );
+    }
 })('fal_protect');
