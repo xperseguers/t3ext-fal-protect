56405c336be7487e603aadcd6d8af0e959085acf
Xavier Perseguers <xavier@causal.ch>
2024-11-28 11:17:35 +0100

    [TASK] Raise compatibility with PHP 8.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Causal Sàrl',
     'constraints' => [
         'depends' => [
-            'php' => '7.2.0-8.3.99',
+            'php' => '7.2.0-8.4.99',
             'typo3' => '10.4.0-13.4.99'
         ],
         'conflicts' => [],
